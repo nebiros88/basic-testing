5247dd6e24a4b5726b371307dd1e1aa3315292c0
nebiros88 <nebiros_88@mail.ru>
2023-06-30 00:30:54 +0200

    feat: add test cases for error-handling-async and test-class

--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,37 +1,83 @@
 // Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+import 'lodash';
+import {
+  getBankAccount,
+  BankAccount,
+  InsufficientFundsError,
+  TransferFailedError,
+} from './index';
 
 describe('BankAccount', () => {
+  const initBalance = 100;
+  const amountMoreThanInitBalance = 120;
+  const amountLessThanInitBalance = 80;
+  const amountForDeposit = 50;
+  let account: BankAccount;
+
+  // jest.mock('lodash/random', () => jest.fn(() => 10));
+
+  jest.mock('lodash', () => {
+    const module = jest.requireActual('lodash');
+    module.random = jest.fn(() => 10);
+    return module;
+  });
+
+  beforeEach(() => {
+    account = getBankAccount(initBalance);
+    jest.clearAllMocks();
+  });
+
   test('should create account with initial balance', () => {
-    // Write your test here
+    expect(account.getBalance()).toBe(initBalance);
   });
 
   test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
-    // Write your test here
+    try {
+      account.withdraw(amountMoreThanInitBalance);
+    } catch (error) {
+      expect(error).toBeInstanceOf(InsufficientFundsError);
+    }
   });
 
   test('should throw error when transferring more than balance', () => {
-    // Write your test here
+    const accoutToTransfer: BankAccount = new BankAccount(initBalance);
+    try {
+      account.transfer(amountMoreThanInitBalance, accoutToTransfer);
+    } catch (error) {
+      expect(error).toBeInstanceOf(InsufficientFundsError);
+    }
   });
 
   test('should throw error when transferring to the same account', () => {
-    // Write your test here
+    try {
+      account.transfer(amountLessThanInitBalance, account);
+    } catch (error) {
+      expect(error).toBeInstanceOf(TransferFailedError);
+    }
   });
 
   test('should deposit money', () => {
-    // Write your test here
+    account.deposit(amountForDeposit);
+    expect(account.getBalance()).toBe(initBalance + amountForDeposit);
   });
 
   test('should withdraw money', () => {
-    // Write your test here
+    account.withdraw(amountLessThanInitBalance);
+    expect(account.getBalance()).toBe(initBalance - amountLessThanInitBalance);
   });
 
   test('should transfer money', () => {
-    // Write your test here
+    const accoutToTransfer: BankAccount = new BankAccount(initBalance);
+    account.transfer(amountLessThanInitBalance, accoutToTransfer);
+    expect(accoutToTransfer.getBalance()).toBe(
+      initBalance + amountLessThanInitBalance,
+    );
   });
 
   test('fetchBalance should return number in case if request did not failed', async () => {
-    // Write your tests here
+    const fetchResult = await account.fetchBalance();
+    console.log('TYPEOF', typeof fetchResult);
+    expect(fetchResult).toBeInstanceOf(Number);
   });
 
   test('should set new balance if fetchBalance returned number', async () => {
